1791b021590bf03363ba90086b1f638a7122b6d9
Kim Allen <kimmarieallen@gmail.com>
2015-12-18 14:03:44 -0800

    Add generate_board method to game using recursion

--- app/models/game.rb
+++ app/models/game.rb
@@ -6,8 +6,21 @@ class Game < ActiveRecord::Base
 
   # takes in an array of clues
   # if there are less than 25 clues
-  # def generate_board(clues)
-  #   clues.shuffle
-  #   clues
-  # end
+  def generate_board(clues)
+    clues.shuffle!
+    if clues.length == 25
+      return clues
+    else
+      if clues.length > 25
+        return clues[0...25]
+      else
+        remaining = 25 % clues.length
+        if remaining == 0
+          generate_board(clues * 25/clues.length)
+        else
+          generate_board(clues + clues[0.. .remaining])
+        end
+      end
+    end
+  end
 end
